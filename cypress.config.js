9a2a46bd930cabc5cb856c4814a47c75c587725e
MUYUNDO BRIAN <muyundobrian@gmail.com>
2025-07-16 12:51:04 +0300

    included cypress cloud project ID
    '

--- cypress.config.js
+++ cypress.config.js
@@ -3,9 +3,10 @@ const { addMatchImageSnapshotPlugin } = require('cypress-image-snapshot/plugin')
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-            addMatchImageSnapshotPlugin(on, config)
+    projectId: "sz8cqz",
 
+    setupNodeEvents(on, config) {
+      addMatchImageSnapshotPlugin(on, config)
       // implement node event listeners here
     },
   },
