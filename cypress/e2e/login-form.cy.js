1e5745e02951dbde8a39a65630bd64aa90c3641f
MUYUNDO BRIAN <muyundobrian@gmail.com>
2025-07-16 14:43:49 +0300

    updated failure threshhold for for login page visualness

--- cypress/e2e/login-form.cy.js
+++ cypress/e2e/login-form.cy.js
@@ -11,13 +11,13 @@ describe('Login Page Visual Testing', () => {
         cy.get('.orangehrm-login-footer', {timeout: 10000}).should('be.visible')
     
     })
+
     it('Confirm that login page is the same visually', () => {
         cy.get('.oxd-text--h5', {timeout: 10000}).contains('Login').should('be.visible')//wait for load
         cy.get('body').matchImageSnapshot('Login-Page', {
         failureThreshold: 0.02,
         failureThresholdType: 'percent'
         }) 
-
     })
 
     it('Should have correct logo image and should be visible', () => {
@@ -28,10 +28,27 @@ describe('Login Page Visual Testing', () => {
         })
         .and('have.attr', 'src')
         .and('include', 'ohrm_logo.png')
-})
+    })
+
     it('should have consistent logo appearance', () => {
         cy.get('.orangehrm-login-logo > img').should('be.visible')
         cy.matchImageSnapshot('Login-Logo')
+    })
+
+    it('Verify error message for invalid credentials and ensure login page appearance remains consistent after error is displayed', () => {
+        cy.get('input[name="username"]').type('invalidUser')
+        cy.get('input[name="password"]').type('invalidPass')
+        cy.get('button[type="submit"]').click()
+        cy.get('.oxd-alert-content', {timeout: 10000}).should('be.visible')
+            .and('contain', 'Invalid credentials')
+        
+        cy.matchImageSnapshot('Login-Error-Message')
+    })
+
+    it('Verify that the footer text is visible and matches the expected text', () => {
+        cy.get('.orangehrm-login-footer').should('be.visible')
+            .and('contain', 'OrangeHRM')
+            .and('contain', '© 2005 - 2025 OrangeHRM, Inc. All rights reserved.')
     })
 
 })
